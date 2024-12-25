16b88a03718c8313160a925ce4ff9b5f0265ee04
SyedaMehakShah <160258619+SyedaMehakShah@users.noreply.github.com>
2024-12-25 20:11:42 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Banner from "./components/banner";
 import Navbar from "./components/navbar";
 import Main from "./components/main";
-import CommentSection from "./components/comment";
+
 
 export default function  Home() {
   return (
